1fa1981623708c94c55a61e340f3cba153eaf71d
mehul9595 <mehul9595@gmail.com>
2021-01-09 22:34:49 +0800

    code to execute procedure add route call in function api impl in index

--- helpers/utils.js
+++ helpers/utils.js
@@ -1,72 +1,71 @@
-const Connection = require('tedious').Connection;
-const Request = require('tedious').Request;
-const TYPES = require('tedious').TYPES;
+const Connection = require("tedious").Connection;
+const Request = require("tedious").Request;
+const TYPES = require("tedious").TYPES;
 
 const exectueSQL = (context, verb, payload) => {
-    var result = "";
-    paramPayload = payload ? JSON.stringify(payload) : "";
+  var result = "";
+  const paramPayload = payload != null ? JSON.stringify(payload) : "";
+  const query = `[AIA].${verb}_registrations`;
 
-    console.log('executing from executeSQL');
+  console.log("executing from executeSQL");
 
-    // setup connection details
-    const _connection = new Connection({
-        server: process.env["db_server"],
-        authentication : {
-            type: 'default',
-            options: {
-                userName: process.env["db_user"],
-                password: process.env["db_password"]
-            }
-        },
-        options: {
-            database: process.env["db_database"],
-            encrypt: true,
-            validateBulkLoadParameters: true
-        }        
+  // setup connection details
+  const _connection = new Connection({
+    server: process.env["db_server"],
+    authentication: {
+      type: "default",
+      options: {
+        userName: process.env["db_user"],
+        password: process.env["db_password"]
+      },
+    },
+    options: {
+      database: process.env["db_database"],
+      encrypt: true,
+      validateBulkLoadParameters: true
+    },
+  });
+
+
+  // configure db request
+  const _request = new Request(query, (err) => {
+    if (err) {
+      context.res.status = 500;
+      context.res.body = err; // chanage this to error message
+      console.log(err);
+    } else {
+      context.res = {
+        body: result,
+        status: 200,
+      };
+    }
+    context.done(); // completes functions and return to the client
+  });
+
+  if (typeof payload === 'boolean') {
+    _request.addParameter("payload", TYPES.Bit, payload, Infinity);
+  } else {
+    _request.addParameter("payload", TYPES.NVarChar, paramPayload, Infinity);
+  }
+
+  _connection.on("connect", (err) => {
+    if (err) {
+      context.res.status = 500;
+      context.res.body = err; // chanage this to error message
+      console.log(err);
+      context.done(); // completes functions and return to the client
+    } else {
+      _connection.callProcedure(_request);
+    }
+  });
+
+  _request.on("row", (columns) => {
+    columns.forEach((col) => {
+      result += col.value;
     });
+  });
 
-    // configure db request
-    const _request = new Request('[AIA].post_registrations', (err) => {
-        if (err)
-        {
-            context.res.status = 500;
-            context.res.body = err; // chanage this to error message
-            console.log(err);
-        }
-        else {
-            context.res = {
-                body: result,
-                status: 200
-            }
-        }
-        context.done(); // completes functions and return to the client
-    });
-    if (paramPayload)
-        _request.addParameter('payload', TYPES.NVarChar, paramPayload, Infinity);
-
-    _connection.on('connect', (err) => {
-        if (err)
-        {
-            context.res.status = 500;
-            context.res.body = err; // chanage this to error message
-            console.log(err);
-            context.done(); // completes functions and return to the client
-        }
-        else {
-            _connection.callProcedure(_request);
-        }
-    });
-
-    _request.on('row', columns => {
-        columns.forEach(col => {
-            result += col.value;
-        });
-    });
-
-    _connection.connect();
-
-
+  _connection.connect();
 };
-
 
 module.exports.exectueSQL = exectueSQL;
--- registrationfunc/index.js
+++ registrationfunc/index.js
@@ -1,23 +1,27 @@
 const { exectueSQL } = require('../helpers/utils');
 
+const parseBool = (params) => {
+    return !((params === 'false') || (params === '0') || (params === '') || (params === undefined));
+}
+
+
 registrationAPI = function (context, req) {
     const method = req.method.toLowerCase();
     var payload = null;
-    var entry = "Registrations";
-
     /*
     TODO: 
         1) Get count of registrations
         2) post registration forms
      */
-    console.log(req.body);
+
     switch (method) {
         case "get":
-            payload = req.params.id || req.query.id || req.body;
-            // TBD
+            payload = parseBool(req.params.all || req.query.all);
+            console.log(`'param: ${payload}'`);
             break;
         case "post":
             payload = req.body;
+            console.log(`'body: ${payload}'`);
             break;
     }    
 
